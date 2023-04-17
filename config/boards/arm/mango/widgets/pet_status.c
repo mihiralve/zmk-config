1514a4f92ee4f8b7e81a1bfe4567aebfc9ccf30a
HellTM <lisca94@gmail.com>
2023-04-17 01:11:41 +0100

    removed unused parameter

--- config/boards/arm/mango/widgets/pet_status.c
+++ config/boards/arm/mango/widgets/pet_status.c
@@ -125,7 +125,7 @@ void init_anim(struct zmk_widget_pet_status *widget) {
 }
 
 int zmk_widget_pet_status_init(struct zmk_widget_pet_status *widget, lv_obj_t *parent) {
-    widget->obj = lv_img_create(parent, NULL);
+    widget->obj = lv_img_create(parent);
 
     lv_img_set_auto_size(widget->obj, true);
 
